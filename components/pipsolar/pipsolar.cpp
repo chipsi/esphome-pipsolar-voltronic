153128f6e243c0ab71243c7645802732844f8a14
chipsi <30642227+chipsi@users.noreply.github.com>
2023-03-10 04:19:28 +0100

    Add files via upload

--- components/pipsolar/pipsolar.cpp
+++ components/pipsolar/pipsolar.cpp
@@ -477,7 +477,7 @@ void Pipsolar::loop() {
         // 000.0 00.0 239.5 50.0 0144 0014 002 422 53.10 004 100 0034 02.2 206.0 00.00 00000 00010110 00 00 00457 110 (EASUN SML-II 48V 5500W)
         sscanf(                                                                                                  // NOLINT
             tmp,                                                                                                 // NOLINT
-            "(%f %f %f %f %d %d %d %d %f %d %d %d %f %f %f %d %1d%1d%1d%1d%1d%1d%1d%1d %f %d %d %1d%1d%1d",      // NOLINT
+            "(%f %f %f %f %d %d %d %d %f %d %d %d %f %f %f %d %1d%1d%1d%1d%1d%1d%1d%1d %d %d %d %1d%1d%1d",      // NOLINT
             // 225.8   |              |            |                |              |               |       1     // NOLINT
             //   49.9  |              |            |                |              |               |       2     // NOLINT
             //      225.8             |            |                |              |               |       3     // NOLINT
@@ -535,8 +535,8 @@ void Pipsolar::loop() {
             &value_ac_charging_status_,                                                       //          24     // NOLINT
             &value_battery_voltage_offset_for_fans_on_,                                       //          25     // NOLINT
             &value_eeprom_version_,                                                           //          26     // NOLINT
-            &value_pv_input_current_for_battery_ * &value_pv_input_voltage_,                  //          27     // NOLINT
-//            &value_pv_charging_power_,                                                        //          27     // NOLINT
+//            &value_pv_input_current_for_battery_ * &value_pv_input_voltage_,                  //          27     // NOLINT
+            &value_pv_charging_power_,                                                        //          27     // NOLINT
             &value_charging_to_floating_mode_,                                                //          28     // NOLINT
             &value_switch_on_,                                                                //          29     // NOLINT
             &value_dustproof_installed_                                                       //          30     // NOLINT
