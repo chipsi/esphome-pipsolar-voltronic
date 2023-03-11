d50cb0b134c234a0e683d1b690f932733b50c508
chipsi <30642227+chipsi@users.noreply.github.com>
2023-03-11 17:12:55 +0100

    1

--- components/pipsolar/sensor/__init__.py
+++ components/pipsolar/sensor/__init__.py
@@ -23,8 +23,8 @@ from .. import CONF_PIPSOLAR_ID, PIPSOLAR_COMPONENT_SCHEMA
 DEPENDENCIES = ["uart"]
 
 # QPIRI sensors
-CONF_GRID_RATING_VOLTAGE = "grid_rating_voltage"
-CONF_GRID_RATING_CURRENT = "grid_rating_current"
+CONF_AC_INPUT_RATING_VOLTAGE = "ac_input_rating_voltage"
+CONF_AC_INPUT_RATING_CURRENT = "ac_input_rating_current"
 CONF_AC_OUTPUT_RATING_VOLTAGE = "ac_output_rating_voltage"
 CONF_AC_OUTPUT_RATING_FREQUENCY = "ac_output_rating_frequency"
 CONF_AC_OUTPUT_RATING_CURRENT = "ac_output_rating_current"
@@ -32,22 +32,22 @@ CONF_AC_OUTPUT_RATING_APPARENT_POWER = "ac_output_rating_apparent_power"
 CONF_AC_OUTPUT_RATING_ACTIVE_POWER = "ac_output_rating_active_power"
 CONF_BATTERY_RATING_VOLTAGE = "battery_rating_voltage"
 CONF_BATTERY_RECHARGE_VOLTAGE = "battery_recharge_voltage"
+CONF_BATTERY_REDISCHARGE_VOLTAGE = "battery_redischarge_voltage"
 CONF_BATTERY_UNDER_VOLTAGE = "battery_under_voltage"
 CONF_BATTERY_BULK_VOLTAGE = "battery_bulk_voltage"
 CONF_BATTERY_FLOAT_VOLTAGE = "battery_float_voltage"
 CONF_BATTERY_TYPE = "battery_type"
-CONF_CURRENT_MAX_AC_CHARGING_CURRENT = "current_max_ac_charging_current"
-CONF_CURRENT_MAX_CHARGING_CURRENT = "current_max_charging_current"
+CONF_MAX_AC_CHARGING_CURRENT = "max_ac_charging_current"
+CONF_MAX_CHARGING_CURRENT = "max_charging_current"
 CONF_INPUT_VOLTAGE_RANGE = "input_voltage_range"
 CONF_OUTPUT_SOURCE_PRIORITY = "output_source_priority"
 CONF_CHARGER_SOURCE_PRIORITY = "charger_source_priority"
 CONF_PARALLEL_MAX_NUM = "parallel_max_num"
 CONF_MACHINE_TYPE = "machine_type"
 CONF_TOPOLOGY = "topology"
-CONF_OUTPUT_MODE = "output_mode"
-CONF_BATTERY_REDISCHARGE_VOLTAGE = "battery_redischarge_voltage"
-CONF_PV_OK_CONDITION_FOR_PARALLEL = "pv_ok_condition_for_parallel"
-CONF_PV_POWER_BALANCE = "pv_power_balance"
+CONF_OUTPUT_MODEL_SETTING = "output_model_setting"
+CONF_SOLAR_POWER_PRIORITY = "solar_power_priority"
+CONF_MPPT_STRING = "mppt_string"
 
 # QPIGS sensors
 
@@ -58,30 +58,35 @@ CONF_AC_OUTPUT_FREQUENCY = "ac_output_frequency"
 CONF_AC_OUTPUT_APPARENT_POWER = "ac_output_apparent_power"
 CONF_AC_OUTPUT_ACTIVE_POWER = "ac_output_active_power"
 CONF_OUTPUT_LOAD_PERCENT = "output_load_percent"
+CONF_BATTERY_VOLTAGE = "battery_voltage"
+CONF_BATTERY_VOLTAGE_SCC = "battery_voltage_scc"
+CONF_BATTERY_VOLTAGE_SCC2 = "battery_voltage_scc2"
+CONF_BATTERY_DISCHARGE_CURRENT = "battery_discharge_current"
 CONF_BATTERY_CHARGING_CURRENT = "battery_charging_current"
 CONF_BATTERY_CAPACITY_PERCENT = "battery_capacity_percent"
 CONF_INVERTER_HEAT_SINK_TEMPERATURE = "inverter_heat_sink_temperature"
-CONF_PV_INPUT_CURRENT_FOR_BATTERY = "pv_input_current_for_battery"
+CONF_MPPT1_CHARGER_TEMPERATURE = "mppt1_charger_temperature"
+CONF_MPPT2_CHARGER_TEMPERATURE = "mppt2_charger_temperature"
 CONF_PV1_INPUT_POWER = "pv1_input_power"
-CONF_PV_INPUT_VOLTAGE = "pv_input_voltage"
+CONF_PV2_INPUT_POWER = "pv2_input_power"
 CONF_PV1_INPUT_VOLTAGE = "pv1_input_voltage"
-CONF_BATTERY_VOLTAGE_SCC = "battery_voltage_scc"
-CONF_BATTERY_DISCHARGE_CURRENT = "battery_discharge_current"
-CONF_ADD_SBU_PRIORITY_VERSION = "add_sbu_priority_version"
-CONF_CONFIGURATION_STATUS = "configuration_status"
-CONF_SCC_FIRMWARE_VERSION = "scc_firmware_version"
-CONF_BATTERY_VOLTAGE_OFFSET_FOR_FANS_ON = "battery_voltage_offset_for_fans_on"
-CONF_EEPROM_VERSION = "eeprom_version"
-CONF_PV_CHARGING_POWER = "pv_charging_power"
+CONF_PV2_INPUT_VOLTAGE = "pv2_input_voltage"
+CONF_SETTING_VALUE_CONFIGURATION_STATE = "setting_value_configuration_state"
+CONF_MPPT1_CHARGER_STATUS = "mppt1_charger_status"
+CONF_MPPT2_CHARGER_STATUS = "mppt2_charger_status"
+CONF_LOAD_CONNECTION = "load_connection"
+CONF_BATTERY_POWER_DIRECTION = "battery_power_direction"
+CONF_LINE_POWER_DIRECTION = "line_power_direction"
+CONF_LOCAL_PARALLEL_ID = "local_parallel_id"
 
 
 TYPES = {
-    CONF_GRID_RATING_VOLTAGE: sensor.sensor_schema(
+    CONF_AC_INPUT_RATING_VOLTAGE: sensor.sensor_schema(
         unit_of_measurement=UNIT_VOLT,
         accuracy_decimals=1,
         device_class=DEVICE_CLASS_VOLTAGE,
     ),
-    CONF_GRID_RATING_CURRENT: sensor.sensor_schema(
+    CONF_AC_INPUT_RATING_CURRENT: sensor.sensor_schema(
         unit_of_measurement=UNIT_AMPERE,
         accuracy_decimals=1,
         device_class=DEVICE_CLASS_CURRENT,
@@ -138,12 +143,12 @@ TYPES = {
     CONF_BATTERY_TYPE: sensor.sensor_schema(
         accuracy_decimals=1,
     ),
-    CONF_CURRENT_MAX_AC_CHARGING_CURRENT: sensor.sensor_schema(
+    CONF_MAX_AC_CHARGING_CURRENT: sensor.sensor_schema(
         unit_of_measurement=UNIT_AMPERE,
         accuracy_decimals=1,
         device_class=DEVICE_CLASS_CURRENT,
     ),
-    CONF_CURRENT_MAX_CHARGING_CURRENT: sensor.sensor_schema(
+    CONF_MAX_CHARGING_CURRENT: sensor.sensor_schema(
         unit_of_measurement=UNIT_AMPERE,
         accuracy_decimals=0,
         device_class=DEVICE_CLASS_CURRENT,
@@ -166,7 +171,7 @@ TYPES = {
     CONF_TOPOLOGY: sensor.sensor_schema(
         accuracy_decimals=1,
     ),
-    CONF_OUTPUT_MODE: sensor.sensor_schema(
+    CONF_OUTPUT_MODEL_SETTING: sensor.sensor_schema(
         accuracy_decimals=1,
     ),
     CONF_BATTERY_REDISCHARGE_VOLTAGE: sensor.sensor_schema(
@@ -174,12 +179,7 @@ TYPES = {
         accuracy_decimals=1,
         device_class=DEVICE_CLASS_VOLTAGE,
     ),
-    CONF_PV_OK_CONDITION_FOR_PARALLEL: sensor.sensor_schema(
-        accuracy_decimals=1,
-    ),
-    CONF_PV_POWER_BALANCE: sensor.sensor_schema(
-        accuracy_decimals=1,
-    ),
+/////////////////////////////////////////////////////////////////////////////
     CONF_GRID_VOLTAGE: sensor.sensor_schema(
         unit_of_measurement=UNIT_VOLT,
         accuracy_decimals=1,
@@ -212,11 +212,6 @@ TYPES = {
     CONF_OUTPUT_LOAD_PERCENT: sensor.sensor_schema(
         unit_of_measurement=UNIT_PERCENT,
         accuracy_decimals=1,
-    ),
-    CONF_BUS_VOLTAGE: sensor.sensor_schema(
-        unit_of_measurement=UNIT_VOLT,
-        accuracy_decimals=1,
-        device_class=DEVICE_CLASS_VOLTAGE,
     ),
     CONF_BATTERY_VOLTAGE: sensor.sensor_schema(
         unit_of_measurement=UNIT_VOLT,
