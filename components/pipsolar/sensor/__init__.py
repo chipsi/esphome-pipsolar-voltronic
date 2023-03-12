ea806de7f91cf62fb783fd45f5922b5d35f443a5
chipsi <30642227+chipsi@users.noreply.github.com>
2023-03-12 03:07:56 +0100

    Update __init__.py

--- components/pipsolar/sensor/__init__.py
+++ components/pipsolar/sensor/__init__.py
@@ -50,7 +50,6 @@ CONF_SOLAR_POWER_PRIORITY = "solar_power_priority"
 CONF_MPPT_STRING = "mppt_string"
 
 # QPIGS sensors
-
 CONF_GRID_VOLTAGE = "grid_voltage"
 CONF_GRID_FREQUENCY = "grid_frequency"
 CONF_AC_OUTPUT_VOLTAGE = "ac_output_voltage"
@@ -76,6 +75,7 @@ CONF_MPPT1_CHARGER_STATUS = "mppt1_charger_status"
 CONF_MPPT2_CHARGER_STATUS = "mppt2_charger_status"
 CONF_LOAD_CONNECTION = "load_connection"
 CONF_BATTERY_POWER_DIRECTION = "battery_power_direction"
+CONF_DC_AC_POWER_DIRECTION = "dc_ac_power_direction"
 CONF_LINE_POWER_DIRECTION = "line_power_direction"
 CONF_LOCAL_PARALLEL_ID = "local_parallel_id"
 
@@ -154,25 +154,25 @@ TYPES = {
         device_class=DEVICE_CLASS_CURRENT,
     ),
     CONF_INPUT_VOLTAGE_RANGE: sensor.sensor_schema(
-        accuracy_decimals=1,
+        accuracy_decimals=0,
     ),
     CONF_OUTPUT_SOURCE_PRIORITY: sensor.sensor_schema(
-        accuracy_decimals=1,
+        accuracy_decimals=0,
     ),
     CONF_CHARGER_SOURCE_PRIORITY: sensor.sensor_schema(
-        accuracy_decimals=1,
+        accuracy_decimals=0,
     ),
     CONF_PARALLEL_MAX_NUM: sensor.sensor_schema(
-        accuracy_decimals=1,
+        accuracy_decimals=0,
     ),
     CONF_MACHINE_TYPE: sensor.sensor_schema(
-        accuracy_decimals=1,
+        accuracy_decimals=0,
     ),
     CONF_TOPOLOGY: sensor.sensor_schema(
-        accuracy_decimals=1,
+        accuracy_decimals=0,
     ),
     CONF_OUTPUT_MODEL_SETTING: sensor.sensor_schema(
-        accuracy_decimals=1,
+        accuracy_decimals=0,
     ),
     CONF_BATTERY_REDISCHARGE_VOLTAGE: sensor.sensor_schema(
         unit_of_measurement=UNIT_VOLT,
@@ -202,34 +202,49 @@ TYPES = {
     ),
     CONF_AC_OUTPUT_APPARENT_POWER: sensor.sensor_schema(
         unit_of_measurement=UNIT_VOLT_AMPS,
-        accuracy_decimals=1,
+        accuracy_decimals=0,
     ),
     CONF_AC_OUTPUT_ACTIVE_POWER: sensor.sensor_schema(
         unit_of_measurement=UNIT_WATT,
-        accuracy_decimals=1,
+        accuracy_decimals=0,
         device_class=DEVICE_CLASS_POWER,
     ),
     CONF_OUTPUT_LOAD_PERCENT: sensor.sensor_schema(
         unit_of_measurement=UNIT_PERCENT,
-        accuracy_decimals=1,
+        accuracy_decimals=0,
     ),
     CONF_BATTERY_VOLTAGE: sensor.sensor_schema(
         unit_of_measurement=UNIT_VOLT,
         accuracy_decimals=1,
         device_class=DEVICE_CLASS_VOLTAGE,
     ),
+    CONF_BATTERY_DISCHARGE_CURRENT: sensor.sensor_schema(
+        unit_of_measurement=UNIT_AMPERE,
+        accuracy_decimals=0,
+        device_class=DEVICE_CLASS_CURRENT,
+    ),
     CONF_BATTERY_CHARGING_CURRENT: sensor.sensor_schema(
         unit_of_measurement=UNIT_AMPERE,
-        accuracy_decimals=1,
+        accuracy_decimals=0,
         device_class=DEVICE_CLASS_CURRENT,
     ),
     CONF_BATTERY_CAPACITY_PERCENT: sensor.sensor_schema(
         unit_of_measurement=UNIT_PERCENT,
-        accuracy_decimals=1,
+        accuracy_decimals=0,
     ),
     CONF_INVERTER_HEAT_SINK_TEMPERATURE: sensor.sensor_schema(
         unit_of_measurement=UNIT_CELSIUS,
         accuracy_decimals=1,
+        device_class=DEVICE_CLASS_TEMPERATURE,
+    ),
+    CONF_MPPT1_CHARGER_TEMPERATURE: sensor.sensor_schema(
+        unit_of_measurement=UNIT_CELSIUS,
+        accuracy_decimals=0,
+        device_class=DEVICE_CLASS_TEMPERATURE,
+    ),
+    CONF_MPPT2_CHARGER_TEMPERATURE: sensor.sensor_schema(
+        unit_of_measurement=UNIT_CELSIUS,
+        accuracy_decimals=0,
         device_class=DEVICE_CLASS_TEMPERATURE,
     ),
     CONF_PV1_INPUT_POWER: sensor.sensor_schema(
@@ -237,44 +252,45 @@ TYPES = {
         accuracy_decimals=0,
         device_class=DEVICE_CLASS_POWER,
     ),
-    CONF_PV_INPUT_CURRENT_FOR_BATTERY: sensor.sensor_schema(
-        unit_of_measurement=UNIT_AMPERE,
-        accuracy_decimals=1,
-        device_class=DEVICE_CLASS_CURRENT,
+    CONF_PV2_INPUT_POWER: sensor.sensor_schema(
+        unit_of_measurement=UNIT_WATT,
+        accuracy_decimals=0,
+        device_class=DEVICE_CLASS_POWER,
     ),
-    CONF_PV_INPUT_VOLTAGE: sensor.sensor_schema(
+    CONF_PV1_INPUT_VOLTAGE: sensor.sensor_schema(
         unit_of_measurement=UNIT_VOLT,
         accuracy_decimals=1,
         device_class=DEVICE_CLASS_VOLTAGE,
     ),
-    CONF_PV1_INPUT_VOLTAGE: sensor.sensor_schema(
+    CONF_PV2_INPUT_VOLTAGE: sensor.sensor_schema(
         unit_of_measurement=UNIT_VOLT,
         accuracy_decimals=0,
         device_class=DEVICE_CLASS_VOLTAGE,
     ),
-    CONF_BATTERY_VOLTAGE_SCC: sensor.sensor_schema(
-        unit_of_measurement=UNIT_VOLT,
-        accuracy_decimals=1,
-        device_class=DEVICE_CLASS_VOLTAGE,
+    CONF_SETTING_VALUE_CONFIGURATION_STATE: sensor.sensor_schema(
+        accuracy_decimals=0,
     ),
-    CONF_BATTERY_DISCHARGE_CURRENT: sensor.sensor_schema(
-        unit_of_measurement=UNIT_AMPERE,
-        accuracy_decimals=1,
-        device_class=DEVICE_CLASS_CURRENT,
+    CONF_MPPT1_CHARGER_STATUS: sensor.sensor_schema(
+        accuracy_decimals=0,
     ),
-    CONF_BATTERY_VOLTAGE_OFFSET_FOR_FANS_ON: sensor.sensor_schema(
-        unit_of_measurement=UNIT_VOLT,
-        accuracy_decimals=1,
-        device_class=DEVICE_CLASS_VOLTAGE,
+    CONF_MPPT2_CHARGER_STATUS: sensor.sensor_schema(
+        accuracy_decimals=0,
     ),
-    CONF_EEPROM_VERSION: sensor.sensor_schema(
-        accuracy_decimals=1,
+    CONF_LOAD_CONNECTION: sensor.sensor_schema(
+        accuracy_decimals=0,
     ),
-    CONF_PV_CHARGING_POWER: sensor.sensor_schema(
-        unit_of_measurement=UNIT_WATT,
-        accuracy_decimals=1,
-        device_class=DEVICE_CLASS_POWER,
+    CONF_BATTERY_POWER_DIRECTION: sensor.sensor_schema(
+        accuracy_decimals=0,
     ),
+    CONF_DC_AC_POWER_DIRECTION: sensor.sensor_schema(
+        accuracy_decimals=0,
+    ),
+    CONF_LINE_POWER_DIRECTION: sensor.sensor_schema(
+        accuracy_decimals=0,
+    ),
+    CONF_LOCAL_PARALLEL_ID: sensor.sensor_schema(
+        accuracy_decimals=0,
+    )
 }
 
 CONFIG_SCHEMA = PIPSOLAR_COMPONENT_SCHEMA.extend(
